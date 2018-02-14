9bd07659cdb36c2a65910d38dc22180cb8b61be9
phillipperks <phillipperks@eduroam-190-111.uea.ac.uk>
2018-02-14 13:28:11 +0000

    added a way to set a classifier if DTWI and DTWD classified the same amount correctly

--- src/activityrecognitionproject/Basic_DTWA.java
+++ src/activityrecognitionproject/Basic_DTWA.java
@@ -30,18 +30,44 @@ public class Basic_DTWA implements Classifier {
     private void setClassifier(Instances data) throws Exception{
         int [] S_Success = findSuccesses(data);
         int S_iSuccess =  S_Success[0];
-        int S_dSuccess =  S_Success[1];
-        
-        if(S_iSuccess>=S_dSuccess)
+        int S_dSuccess =  S_Success[3];
+        int S_iSuccess_Badminton = S_Success[2];
+        int S_dSuccess_Badminton = S_Success[5];
+        int S_iSuccess_Squash = S_Success[1];
+        int S_dSuccess_Squash = S_Success[4];
+        //if DTWI has more successfull classifications
+        if(S_iSuccess>S_dSuccess){
             this.DTWA = new DTWI();
-        else
+        }
+        //if DTWD has more successfull classifications
+        else if(S_dSuccess>S_iSuccess){
             this.DTWA = new DTWD();
+        }
+        //if both classsifiers make the same number of correct classifications
+        else{
+            //if DTWI predicted the coreect sport  more
+            if(S_iSuccess_Badminton+S_iSuccess_Squash>S_dSuccess_Badminton+S_dSuccess_Squash){
+               this.DTWA = new DTWI(); 
+            }
+            //if DTWD predicted the correct sport more
+            else if(S_dSuccess_Badminton+S_dSuccess_Squash>S_iSuccess_Badminton+S_iSuccess_Squash){
+                this.DTWA = new DTWD();
+            }
+            //else use DTWI as a default
+            else{
+                this.DTWA = new DTWI(); 
+            }
+        }
         
     }
     
     private int [] findSuccesses(Instances data) throws Exception{
         int S_iSuccess = 0;
         int S_dSuccess = 0;
+        int S_iSuccess_Badminton = 0;
+        int S_dSuccess_Badminton = 0;
+        int S_iSuccess_Squash = 0;
+        int S_dSuccess_Squash = 0;
         Classifier DTWD = new DTWD();
         Classifier DTWI = new DTWI();
         for(int i=0; i<data.numInstances(); i++){
@@ -52,13 +78,29 @@ public class Basic_DTWA implements Classifier {
             if(data.instance(i).classValue() == DTWD.classifyInstance(data.instance(i))){
                 S_dSuccess++;
             }
+            else if((data.instance(i).classValue() == 0 && DTWD.classifyInstance(data.instance(i)) == 1)
+                    || (data.instance(i).classValue() == 1 && DTWD.classifyInstance(data.instance(i)) == 0) ){
+                S_dSuccess_Squash++;
+            }
+            else if((data.instance(i).classValue() == 2 && DTWD.classifyInstance(data.instance(i)) == 3)
+                    || (data.instance(i).classValue() == 3 && DTWD.classifyInstance(data.instance(i)) == 2) ){
+                S_dSuccess_Badminton++;
+            }
+            
             if(data.instance(i).classValue() == DTWI.classifyInstance(data.instance(i))){
                 S_iSuccess++;
             }
-            
-            
+            else if((data.instance(i).classValue() == 0 && DTWD.classifyInstance(data.instance(i)) == 1)
+                    || (data.instance(i).classValue() == 1 && DTWD.classifyInstance(data.instance(i)) == 0) ){
+                S_iSuccess_Squash++;
+            }
+            else if((data.instance(i).classValue() == 2 && DTWD.classifyInstance(data.instance(i)) == 3)
+                    || (data.instance(i).classValue() == 3 && DTWD.classifyInstance(data.instance(i)) == 2) ){
+                S_iSuccess_Badminton++;
+            }
         }
-        int [] S_Success = {S_iSuccess, S_dSuccess};
+        int [] S_Success = {S_iSuccess, S_iSuccess_Squash, S_iSuccess_Badminton,
+            S_dSuccess, S_dSuccess_Squash, S_dSuccess_Badminton};
         return S_Success;
     }
     
