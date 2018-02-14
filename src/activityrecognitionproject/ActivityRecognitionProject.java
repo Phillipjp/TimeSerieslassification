a2a8656c0a05ff9240b2263d8d91bdc5fd0322eb
phillipperks <phillipperks@eduroam-190-111.uea.ac.uk>
2018-02-14 11:37:30 +0000

    updated run methods

--- src/activityrecognitionproject/ActivityRecognitionProject.java
+++ src/activityrecognitionproject/ActivityRecognitionProject.java
@@ -25,8 +25,9 @@ public class ActivityRecognitionProject {
      */
     public static void main(String[] args) throws Exception {
         
-        Instances all = ClassifierTools.loadData("/Users/phillipperks/Desktop/3rd Year Project/ARFF_Files/Cross Validation/Combination/MVMotionUni.arff");
-        
+        Instances all = ClassifierTools.loadData("/Users/phillipperks/Desktop/3rd-Year-Project/ARFF_Files/Cross Validation/Combination/MVMotionUni.arff");
+        String path="/Users/phillipperks/Desktop/3rd-Year-Project/ARFF_Files/Cross Validation/Combination/MVMotionMulti.arff";
+        Instances multiVariate = ClassifierTools.loadData(path);
         int totalFolds = 30;
         
         double aveBayes = 0;
@@ -35,6 +36,10 @@ public class ActivityRecognitionProject {
         double aveRF = 0;
         double aveEuclid = 0;
         double aveRotation = 0;
+        double aveBasicDTW = 0;
+        double aveDTWI = 0;
+        double aveDTWD = 0;
+        
         
         for(int fold=0; fold<totalFolds; fold++){
             Instances[] data = InstanceTools.resampleInstances(all, fold, .5);
@@ -42,12 +47,19 @@ public class ActivityRecognitionProject {
             Instances train = data[0];
             Instances test = data[1];
             
-            aveEuclid = runEuclideanDistance(aveEuclid,test, train, fold);
-            aveBayes = runBayes(aveBayes,test, train, fold);
-            aveKNN = runKNN(aveKNN,test, train, fold, 3 , "KNN3");
-            aveJ48 = runJ48(aveJ48,test, train, fold);
-            aveRotation = runRotationalForest(aveRotation,test, train, fold);
-            aveRF = runRandomForest(aveRF,test, train, fold, 100, "RANDOMFOREST100");
+            Instances[] multiData = InstanceTools.resampleInstances(multiVariate, fold, .5);
+            Instances multiTrain = multiData[0];
+            Instances  multiTest = multiData[1];
+            
+//            aveEuclid = runEuclideanDistance(aveEuclid,test, train, fold);
+//            aveBayes = runBayes(aveBayes,test, train, fold);
+//            aveKNN = runKNN(aveKNN,test, train, fold, 3 , "KNN3");
+//            aveJ48 = runJ48(aveJ48,test, train, fold);
+//            aveRotation = runRotationalForest(aveRotation,test, train, fold);
+//            aveRF = runRandomForest(aveRF,test, train, fold, 100, "RANDOMFOREST100");
+            //aveBasicDTW = runBasicDTW(aveBasicDTW,test, train, fold);
+            aveDTWI = runDTWI(aveDTWI, multiTest, multiTrain, fold);
+            aveDTWD = runDTWD(aveDTWD, multiTest, multiTrain, fold);
 
             
             
@@ -59,8 +71,35 @@ public class ActivityRecognitionProject {
         System.out.println("Average J48: " + aveJ48/totalFolds);
         System.out.println("Average Rotational Forest: " + aveRotation/totalFolds);
         System.out.println("Average Random Forest: " + aveRF/totalFolds);
+        System.out.println("Average Basic DTW: " + aveBasicDTW/totalFolds);
+        System.out.println("Average DTWI: " + aveDTWI/totalFolds);
+        System.out.println("Average DTWD: " + aveDTWD/totalFolds);
         
         
+    }
+    
+    public static double runDTWD(double aveDTWD, Instances test, Instances train, int fold) throws Exception{
+        ClassifierWrapper dtw = new ClassifierWrapper(new DTWD(), test, train);
+        dtw.classifyAllInstances();
+        //dtw.writeCsvFile("DTWITest" + fold, "DTWI");
+        aveDTWD += dtw.getAccuracy();
+        return aveDTWD;
+    }
+    
+    public static double runDTWI(double aveDTWI, Instances test, Instances train, int fold) throws Exception{
+        ClassifierWrapper dtw = new ClassifierWrapper(new DTWI(), test, train);
+        dtw.classifyAllInstances();
+        //dtw.writeCsvFile("DTWITest" + fold, "DTWI");
+        aveDTWI += dtw.getAccuracy();
+        return aveDTWI;
+    }
+    
+    public static double runBasicDTW(double aveBasicDTW, Instances test, Instances train, int fold) throws Exception{
+        ClassifierWrapper dtw = new ClassifierWrapper(new Basic_DTW(), test, train);
+        dtw.classifyAllInstances();
+        dtw.writeCsvFile("basicDTWTest" + fold, "BASICDTW");
+        aveBasicDTW += dtw.getAccuracy();
+        return aveBasicDTW;
     }
     
     public static double runEuclideanDistance(double aveEuclid, Instances test, Instances train, int fold) throws Exception{
